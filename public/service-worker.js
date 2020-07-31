7bf8a260c246fe2580c4659f076470453f1bb17d
deannapi <deannapi.mart@gmail.com>
2020-07-31 10:55:45 -0500

    fetch error

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,81 +1,68 @@
-const APP_PREFIX = 'BudgetTracker-';     
-const VERSION = 'version_01';
+const APP_PREFIX = "BudgetTracker-";
+const VERSION = "version_01";
 const CACHE_NAME = APP_PREFIX + VERSION;
 
 const FILES_TO_CACHE = [
-  './index.html',
-  './css/styles.css',
-  './js/index.js',
-  './js/idb.js',
-  './manifest.json',
-  './icons/icon-512x512.png',
-  './icons/icon-384x384.png',
-  './icons/icon-192x192.png',
-  './icons/icon-152x152.png',
-  './icons/icon-144x144.png',
-  './icons/icon-128x128.png',
-  './icons/icon-96x96.png',
-  './icons/icon-72x72.png'
+  "./index.html",
+  "./css/styles.css",
+  "./js/index.js",
+  "./js/idb.js",
+  "./manifest.json",
+  "./icons/icon-512x512.png",
+  "./icons/icon-384x384.png",
+  "./icons/icon-192x192.png",
+  "./icons/icon-152x152.png",
+  "./icons/icon-144x144.png",
+  "./icons/icon-128x128.png",
+  "./icons/icon-96x96.png",
+  "./icons/icon-72x72.png",
 ];
 
 // install
-self.addEventListener("install", function(evt) {
-  evt.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      console.log("Your files were pre-cached successfully!");
+self.addEventListener("install", function (e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log("installing cache : " + CACHE_NAME);
       return cache.addAll(FILES_TO_CACHE);
     })
   );
-
-  self.skipWaiting();
 });
 
 // activate
-self.addEventListener('activate', function (e) {
+self.addEventListener("activate", function (e) {
   e.waitUntil(
     caches.keys().then(function (keyList) {
       let cacheKeeplist = keyList.filter(function (key) {
         return key.indexOf(APP_PREFIX);
       });
-          cacheKeeplist.push(CACHE_NAME);
+      cacheKeeplist.push(CACHE_NAME);
 
-          return Promise.all(keyList.map(function (key, i) {
-              if (cacheKeeplist.indexOf(key) === -1) {
-              console.log('deleting cache : ' + keyList[i] );
-              return caches.delete(keyList[i]);
-              }
-          }));
+      return Promise.all(
+        keyList.map(function (key, i) {
+          if (cacheKeeplist.indexOf(key) === -1) {
+            console.log("deleting cache : " + keyList[i]);
+            return caches.delete(keyList[i]);
+          }
+        })
+      );
     })
-  )
+  );
 });
 
 // fetch
-self.addEventListener("fetch", evt => {
-    if(evt.request.url.includes('/api/')) {
-        console.log('[Service Worker] Fetch(data)', evt.request.url);
-    
-evt.respondWith(
-                caches.open(CACHE_NAME).then(cache => {
-                return fetch(evt.request)
-                .then(response => {
-                    if (response.status === 200){
-                        cache.put(evt.request.url, response.clone());
-                    }
-                    return response;
-                })
-                .catch(err => {
-                    return cache.match(evt.request);
-                });
-            })
-            );
-            return;
-        }
-
-// evt.respondWith(
-//     caches.open(CACHE_NAME).then( cache => {
-//       return cache.match(evt.request).then(response => {
-//         return response || fetch(evt.request);
-//       });
-//     })
-//   );
+self.addEventListener("fetch", function (e) {
+  console.log("fetch request : " + e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function (request) {
+      if (request) {
+        // if cache is available, respond with cache
+        console.log("responding with cache : " + e.request.url);
+        return request;
+      } else {
+        // if there are no cache, try fetching request
+        console.log("file is not cached, fetching : " + e.request.url);
+        return fetch(e.request);
+      }
+    })
+  );
 });
